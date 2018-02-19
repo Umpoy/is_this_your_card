ea6cc2acccb95a4e7ad70930989c16d509d6c34a
Umpoy <iumpoy@yahoo.com>
2018-02-19 13:13:44 -0800

    testing MVP on live site

--- main.js
+++ main.js
@@ -1,11 +1,74 @@
-//listen to shake event
-var shakeEvent = new Shake({ threshold: 15 });
-shakeEvent.start();
-window.addEventListener('shake', function () {
-    alert("Shaked");
-}, false);
+var first_click;
+var second_click;
+$(document).ready(initialize);
 
-//stop listening
-function stopShake() {
-    shakeEvent.stop();
+
+function initialize() {
+    assign_click_handler();
 }
+
+function assign_click_handler() {
+    $('.suit').on('click', find_value)
+}
+
+function find_value() {
+    if (second_click) {
+        return
+    }
+    if (first_click) {
+        second_click = $(this).attr("id");
+        render_card()
+    }
+    first_click = $(this).attr("id");
+    console.log(first_click)
+}
+
+function render_card() {
+    var suit = null;
+    switch (second_click) {
+        case '2':
+        case '3':
+        case '4':
+            suit = "H";
+            break;
+        case '5':
+        case '6':
+        case '7':
+            suit = "S";
+            break;
+        case '8':
+        case '9':
+        case '10':
+            suit = "C";
+            break;
+        case 'J':
+        case 'Q':
+        case 'K':
+            suit = "D";
+            break;
+    }
+    var img = document.createElement("IMG");
+    img.src = "https://deckofcardsapi.com/static/img/" + first_click + suit + ".png";
+    $('.reveal').html(img);
+    //listen to shake event
+    var shakeEvent = new Shake({ threshold: 15 });
+    shakeEvent.start();
+    window.addEventListener('shake', function () {
+        $(".front").addClass('hide');
+    }, false);
+
+    //stop listening
+    function stopShake() {
+        shakeEvent.stop();
+    }
+}
+
+
+
+
+
+
+
+
+
+
