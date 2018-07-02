676b71ca32e4882355af76585d3bb4281e2c7e82
Umpoy <iumpoy@yahoo.com>
2018-07-01 22:24:00 -0700

    added new vanishing effect along with eaier to test for desktop

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var count = 0;
+var on_mobile = false;
 
 $(document).ready(initialize);
 
@@ -23,33 +24,25 @@ function alert_on_desktop() {
             return false;
         }
     }
-
+    on_mobile = true;
     setInterval(function () {
         if (!check_if_mobile()) {
-            alert('Sorry this application only works on mobile, please close this tab');
+            alert('Sorry this application is ment for mobile only, please close this tab');
         }
     }, 1);
 }
 
 function assign_click_handler() {
+    if (!on_mobile) {
+        $(".card_back").on("click", function () {
+            $(".card_back").addClass('hide');
+        })
+    }
     $("button").on('click', function () {
         $(".hide").removeClass("hide");
         $(".pop_up").addClass("hide");
         render_to_screen();
     });
-    $(".card_back").on("click", function () {
-        $(".card_back").addClass('hide');
-    })
-    // $(".card_front").on("click", function () {
-    //     count++;
-    //     if (count == 5) {
-    //         $(".pop_up").removeClass("hide");
-    //         count = 0;
-    //     }
-    //     setTimeout(function () {
-    //         count = 0;
-    //     }, 10000)
-    // })
 }
 
 function render_to_screen() {
@@ -59,15 +52,12 @@ function render_to_screen() {
     front.src = "https://deckofcardsapi.com/static/img/" + $("#card_value").val() + $("#card_suite").val() + ".png";
     $(".card_front").html(front);
     $(".card_back").prepend('<img src="https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-1_1024x1024.png?v=1523371937" />');
-    $(".card_front").on("click", function () {
-        clear_screen();
+    $(".card_front").on("click", () => {
+        setTimeout(() => {
+            $(".card_front").addClass("hide");
+        }, 2000);;
     })
     call_shake_api();
-}
-
-function clear_screen() {
-    $(".card_back").addClass('hide');
-    $(".card_front").html("");
 }
 
 function call_shake_api() {
